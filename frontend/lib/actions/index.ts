9a5c5683854aab43893bd55fad07daaf0e333920
Sydon1 <64132834+sydon1@users.noreply.github.com>
2025-05-05 13:24:49 +0200

    Delete frontend directory

--- frontend/lib/actions/index.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-import createSupabaseServerClient from "../supabase/reader";
-
-export default async function readUserSession() {
-  const supabase = await createSupabaseServerClient();
-  return supabase.auth.getSession();
-}
